9b12ebf6d808de6e31e3952b79a0c990edf28ac8
Nitesh Mandal <86831279+Niteshmandal19@users.noreply.github.com>
2025-04-24 13:24:05 +0530

    Update server.js
    added public link also

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   'http://localhost:5176',
-  'https://smart-todo-frontend-cwzmcuoa0-nitesh-mandals-projects.vercel.app',
+  'https://smart-todo-frontend-git-main-nitesh-mandals-projects.vercel.app',
   'https://smart-todo-frontend-eo89sttqm-nitesh-mandals-projects.vercel.app'
 ];
 
